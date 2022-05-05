55eeb9928451995d0606222a61502bf958e16436
Shirley Hody <shirley.hody@holidayextras.com>
2022-05-05 12:01:11 +0100

    Add basic routing

---
+++ src/ResultsPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResultsPage = () => {
+  return (
+    <h1>Results</h1>
+  )
+}
+
+export default ResultsPage
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+import './index.css';
 import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
